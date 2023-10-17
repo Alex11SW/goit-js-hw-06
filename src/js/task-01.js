da678b4ae306263d7181fa01d43c6efdf72d220d
alex_shklyar <ford.alex2223@gmail.com>
2023-10-18 00:19:35 +0300

    new

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,9 +1,9 @@
 const categoriesList = document.querySelector('#categories');
 const categoriItem = categoriesList.querySelectorAll('li.item');
 console.log('Number of categories: ', categoriItem.length);
-categoriItem.forEach(categoriItem => {
-    const categTitle = categoriItem.querySelector('h2').textContent;
-    const categElem = categoriItem.querySelector('ul li');
-    console.log('Category: ${categoriItem}');
-    console.log('Elements: ${categElem}');
+categoriItem.forEach(categoriIte => {
+    const categTitle = categoriIte.querySelector('h2').textContent;
+    const categElem = categoriIte.querySelectorAll('ul li');
+    console.log(`Category: ${categTitle}`);
+    console.log(`Elements: ${categElem.length}`);
 });
