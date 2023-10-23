8a7535d17b7d91fc11aed1eec6f23110c1862f46
Alex11SW <136641877+Alex11SW@users.noreply.github.com>
2023-10-23 22:56:34 +0300

    new

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+
+const colorSpan = document.querySelector('.color');
+const changeColorButton = document.querySelector('.change-color');
+
+changeColorButton.addEventListener('click', function () {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
